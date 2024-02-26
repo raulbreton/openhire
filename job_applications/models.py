3597f5bacd9183544061148c144c9f4bf2399e0c
raulbreton <raul.elizalde.breton@gmail.com>
2024-02-26 14:41:08 -0600

    job_applications model modified with recent experience fields

--- job_applications/models.py
+++ job_applications/models.py
@@ -5,6 +5,10 @@ from job_offers.models import JobOffer
 class JobApplication(models.Model):
     candidate = models.ForeignKey(ApplicantProfile, on_delete=models.CASCADE)
     job_offer = models.ForeignKey(JobOffer, on_delete=models.CASCADE)
+    recent_job_title = models.CharField(max_length=100, null=True)
+    recent_job_company = models.CharField(max_length=255, null=True)
+    fecha_inicio = models.DateField(null=True, blank=True)
+    fecha_fin = models.DateField(null=True, blank=True)
     cover_letter = models.TextField()
     resume = models.FileField(upload_to='resumes/')
     application_date = models.DateTimeField(auto_now_add=True)
